acacf20f5c2d5eba66d68d55c59e3cf554c33cee
olhaTeach <91115726+olhaTeach@users.noreply.github.com>
2021-09-27 12:44:25 +0300

    new project

---
+++ src/Test.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int cash = 100;
+        int n = 8;
+        int price = 45;
+
+        int vitraty = n * price;
+
+        if (cash >= vitraty){
+            System.out.println("Йдемо в кіно" + vitraty);
+        } else {
+            System.out.println("Не йдемо в кіно");
+        }
+
+        String vubir = cash >= vitraty ? "Yes" : "No";
+        System.out.println(vubir);
+
+
+
+        if (price % 2 == 1){
+            System.out.println("neparne");
+        }else {
+            System.out.println("parne");
+        }
+
+
+
+    }
+}
